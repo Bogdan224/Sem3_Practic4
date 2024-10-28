1a10abbe32fb41241cdb0fe3867c028dacadc8fb
user2 <user2@DESKTOP-7J13RTJ>
2024-10-28 13:30:49 +0700

    Добавьте файлы проекта.

---
+++ prac 4/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_4
+{
+    internal class Program
+    {
+        static string[] month = { "June", "Jule", "August", "September", "October", "November", "December", "January", "February", "March", "April", "May" };
+        static void Main(string[] args)
+        {
+            //Task10();
+            //Task11();
+            //Task12();
+            Task13();
+        }
+
+       private static void Task10()
+        {
+            
+            int n = 4;
+            var selectmonth = from m in month
+                              where m.Length > n
+                              select m;
+
+            foreach (var m in selectmonth)
+            {
+                Console.WriteLine(m);
+            }
+
+        }
+        private static void Task11()
+        {
+
+            var selectmonth = from m in month
+                              where m == "June" || m == "Jule" || m == "August" || m == "December" || m == "January" || m == "February"
+                              select m;
+            foreach (var m in selectmonth)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        private static void Task12()
+        {
+            var selectmonth = from m in month
+                              orderby m
+                              select m;
+
+            foreach (var m in selectmonth)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        private static void Task13()
+        {
+            int n = 4;
+            var selectmonth = from m in month
+                              where m.Length > n && m.ToLower().Contains("u")
+                              select m;
+            foreach (var m in selectmonth)
+            {
+                Console.WriteLine(m);
+            }
+        }
+     
+        
+    }
+
+    
+    
+
+    
+
+}
+
---
+++ prac 4/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_4
+{
+    public class Student
+    {
+        public string Firstname { get; set; }
+        
+        public string Lastname { get; set; }
+        public string Patronymic { get; set; }
+        private int birth;
+        public int Birth { 
+            get
+            {
+                return birth;
+            }
+            set
+            {
+                if (!(value >= DateTime.Today.Year - 100 && value <= DateTime.Today.Year))
+                    throw new ArgumentException();
+                birth = value;
+            }
+        }
+        private int course;
+        public int Course {
+            get
+            {
+                return course;
+            }
+            set
+            {
+                if (!(value > 0 && value <= 5))
+                    throw new ArgumentException();
+                course = value;
+            }
+        }
+        public string Group { get; set; }
+
+        public Student(string firstname, string lastname, string patronymic, int birth, int course, string group)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Patronymic = patronymic;
+            Birth = birth;
+            Course = course;
+            Group = group;
+        }
+
+
+    }
+}
